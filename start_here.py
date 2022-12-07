e607e934448aa49aa252d4a9c8e6c2f5c2949ada
shcrela <shcrela@gmail.com>
2022-12-07 23:53:34 +0100

    simplified

--- start_here.py
+++ start_here.py
@@ -5,26 +5,15 @@ Created on Thu Jul 15 10:34:02 2021
 
 @author: dejan
 """
-import numpy as np
 from visualize import ShowSelected
-
 from read_WDF import read_WDF
 
 
-
-# Nom de fichier tout avec le chemin pour y accèder:
+# Nom de fichier avec le chemin pour y accèder:
 filename = "data/test.wdf"
-
 # charger le fichier:
 da, img = read_WDF(filename)
-
 # %% Visualiser:
-
 check = ShowSelected(da, epsilon=2)
-
-# %%
-# Pour récupérer les valeurs affichées:
+# %% Pour récupérer les valeurs affichées:
 valeurs = check.imup.get_array()
-
-moyenne = np.mean(valeurs)
-mediane = np.median(valeurs)
